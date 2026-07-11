7e2dd6402e42365143c78b8618bb0715b4d170a3
Faeq-F <faeqfaisal@hotmail.co.uk>
2026-07-11 15:09:40 +0100

    :sparkles: feat: add optional /hide and /h arguments to run cmd actions invisibly

--- TabletFriend/TabletFriend/Actions/CmdAction.cs
+++ TabletFriend/TabletFriend/Actions/CmdAction.cs
@@ -1,4 +1,8 @@
-﻿using System.Diagnostics;
+// Copyright (c) 2026 Faeq-F. Licensed under GPL version 3.
+// Modified from original code by Martenfur, licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace TabletFriend.Actions
@@ -6,17 +10,37 @@ namespace TabletFriend.Actions
 	public class CmdAction : ButtonAction
 	{
 		private readonly string _cmd;
+		private readonly bool _hide;
 
 		public CmdAction(string cmd)
 		{
-			_cmd = cmd;
+			if (cmd.StartsWith("/h ", StringComparison.OrdinalIgnoreCase))
+			{
+				_cmd = cmd.Substring(3).Trim();
+				_hide = true;
+			}
+			else if (cmd.StartsWith("/hide ", StringComparison.OrdinalIgnoreCase))
+			{
+				_cmd = cmd.Substring(6).Trim();
+				_hide = true;
+			}
+			else
+			{
+				_cmd = cmd;
+				_hide = false;
+			}
 		}
 
 		public override Task Invoke()
 		{
 			var process = new Process();
 			var startInfo = new ProcessStartInfo();
-			startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			if (_hide)
+			{
+				startInfo.CreateNoWindow = true;
+				startInfo.UseShellExecute = false;
+				startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			}
 			startInfo.FileName = "cmd.exe";
 			startInfo.Arguments = "/C " + _cmd;
 			process.StartInfo = startInfo;
